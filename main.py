9e3e0e5475e8b81cd162977ba62bcd8effeba116
redcontest <thezharklee@gmail.com>
2026-05-05 19:12:12 +0500

    создана заготовка главного цикла игры

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+import sys
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((900, 600))
+    running = True
+
+    # while running:
+    #     pass
+
+    pygame.quit()
+    sys.exit()
+
+
+if __name__ == '__main__':
+    main()
