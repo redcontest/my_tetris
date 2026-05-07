33ff3f223125fd7b467648cf82a67f89fe0ae069
redcontest <thezharklee@gmail.com>
2026-05-07 20:02:37 +0500

    развёрнута полная структура проекта

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-import pygame
-import sys
-
-
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((900, 600))
-    running = True
-
-    # while running:
-    #     pass
-
-    pygame.quit()
-    sys.exit()
+from my_tetris.src.tetris.game import Game
 
 
 if __name__ == '__main__':
-    main()
+    game = Game()
+    game.start()
